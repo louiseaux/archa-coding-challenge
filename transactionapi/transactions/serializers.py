9b2333e52b3ce48b86d2dc7477afef6aa817e2c2
Ashlyn <ashlyn.barry1@gmail.com>
2025-06-04 12:12:27 +1000

    Add amount and transaction type validators

--- transactionapi/transactions/serializers.py
+++ transactionapi/transactions/serializers.py
@@ -5,3 +5,19 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = ['transaction_type', 'amount', 'description']
+
+    def validate_transaction_type(self, value):
+        '''
+        Check that transaction type is either Deposit or Withdrawal
+        '''
+        if value not in ['DEPOSIT', 'WITHDRAWAL']:
+            raise serializers.ValidationError('Transaction type must be either "DEPOSIT" or "WITHDRAWAL"')
+        return value
+
+    def validate_amount(self, value):
+        '''
+        Check that amount is positive
+        '''
+        if value <= 0:
+            raise serializers.ValidationError('Amount must be positive.')
+        return value
